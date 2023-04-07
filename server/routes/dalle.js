045d2aa7d782ca09623f6087ad8967ab254de908
Mani Yeganeh <yeganehmani@gmail.com>
2023-04-07 16:28:53 +0330

    server finalized

--- server/routes/dalle.js
+++ server/routes/dalle.js
@@ -17,4 +17,22 @@ const openai = new OpenAIApi(config)
 router.route("/").get((req, res) => {
     res.send("Hello from DALL_E")
 })
+
+router.route("/").post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json"
+        })
+        const image = aiResponse.data.date[0].b64_json;
+        res.status(200).json({ photo: image })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send(err?.response.data.error.message)
+    }
+})
 export default router
