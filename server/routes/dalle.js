f536aec35a8d3547108cbefd343a84bf8e3c0396
Mani Yeganeh <yeganehmani@gmail.com>
2023-04-09 11:53:36 +0330

    post routes added

--- server/routes/dalle.js
+++ server/routes/dalle.js
@@ -21,13 +21,15 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
     try {
         const { prompt } = req.body;
+        console.log(prompt);
         const aiResponse = await openai.createImage({
             prompt,
             n: 1,
             size: "1024x1024",
             response_format: "b64_json"
         })
-        const image = aiResponse.data.date[0].b64_json;
+        const image = aiResponse.data.data[0].b64_json;
+        console.log(image);
         res.status(200).json({ photo: image })
     }
     catch (err) {
